4a0c3fc020219902a4409a157ba5e8b0c3c2d92e
Cristian <cristian.vega08@gmail.com>
2019-09-05 23:10:11 -0500

    game working

--- src/code.ts
+++ src/code.ts
@@ -1,26 +1,68 @@
+import { interval, fromEvent, combineLatest, BehaviorSubject } from 'rxjs';
+import { scan, startWith, map, takeWhile, switchMap } from 'rxjs/operators';
+import { State, Letter, Letters } from './interfaces';
 
-import { Observable as Rx } from "rxjs";
+const randomLetter = () => String.fromCharCode(
+  Math.random() * ('z'.charCodeAt(0) - 'a'.charCodeAt(0)) + 'a'.charCodeAt(0));
+const levelChangeThreshold = 20;
+const speedAdjust = 50;
+const endThreshold = 15;
+const gameWidth = 30;
 
-import { addMessage, printMessage } from './utils';
+const intervalSubject = new BehaviorSubject(600);
 
-const obs = Rx.create((observer: any) => {
-  observer.next('Primer mensaje');
-  observer.next('Segundo mensaje');
-  setInterval(() => {
-    observer.next('Nuevo mensaje');
-  },2000);
-})
+const letters$ = intervalSubject.pipe(
+  switchMap(i => interval(i)
+    .pipe(
+      scan<number, Letters>((letters) => ({
+        intrvl: i,
+        ltrs: [({
+          letter: randomLetter(),
+          yPos: Math.floor(Math.random() * gameWidth)
+        }), ...letters.ltrs]
+      }), { ltrs: [], intrvl: 0 })
+    )));
 
-const observer1 = obs.subscribe(
-  (x: any) => addMessage(x),
-  (e: any) => printMessage(e),
-  () => printMessage('Mensaje completo', 'complete')
+const keys$ = fromEvent(document, 'keydown')
+  .pipe(
+    startWith({ key: '' }),
+    map((e: KeyboardEvent) => e.key)
+  );
+
+const node = document.createElement('div');
+const renderGame = (state: State) => {
+  const output = document.getElementById('output')
+  node.innerHTML = `Score: ${state.score}, Level: ${state.level} <br/>`,
+  state.letters.forEach(l => node.innerHTML +=
+    '&nbsp'.repeat(l.yPos) + l.letter + '<br/>'),
+  node.innerHTML +=
+  '<br/>'.repeat(endThreshold - state.letters.length - 1) + '-'.repeat(gameWidth)
+  return output.appendChild(node)
+};
+
+const renderGameOver = () => node.innerHTML += '<br/>GAME OVER!';
+const noop = () => { };
+
+const game$ = combineLatest(keys$, letters$).pipe(
+  scan<[string, Letters], State>((state, [key, letters]) => (
+    letters.ltrs[letters.ltrs.length - 1]
+      && letters.ltrs[letters.ltrs.length - 1].letter === key
+      ? (state.score = state.score + 1, letters.ltrs.pop())
+      : noop,
+    state.score > 0 && state.score % levelChangeThreshold === 0
+      ? (
+        letters.ltrs = [],
+        state.level = state.level + 1,
+        state.score = state.score + 1,
+        intervalSubject.next(letters.intrvl - speedAdjust))
+      : noop,
+    ({ score: state.score, letters: letters.ltrs, level: state.level })),
+    { score: 0, letters: [], level: 1 }),
+  takeWhile(state => state.letters.length < endThreshold),
 )
 
-obs.subscribe(
-  (x: any) => addMessage(x, true),
-  (e: any) => printMessage(e),
-  () => printMessage('Mensaje completo', 'complete')
-)
-
-// setTimeout(() => observer1.unsubscribe(), 5000);
+game$.subscribe(
+  renderGame,
+  noop,
+  renderGameOver
+);
