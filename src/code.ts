f1b6eb8e2c489d614e6ee7dfa8bced8fc207e677
Cristian <cristian.vega08@gmail.com>
2019-09-06 23:23:05 -0500

    cold ready

--- src/code.ts
+++ src/code.ts
@@ -1,26 +1,27 @@
-import { interval, from } from 'rxjs';
+import { Observable } from 'rxjs';
+import { share } from 'rxjs/operators';
 import { addMessage, printMessage } from './utils';
- 
-const observable1 = interval(400);
-const observable2 = interval(300);
- 
-const subscription = observable1.subscribe(x => addMessage(`first: ${x}`));
-const childSubscription = observable2.subscribe(x => addMessage(`second: ${x}`, true))
 
-subscription.add(childSubscription);
- 
+const obs = Observable.create((observer: any) => {
+  observer.next('Primer mensaje');
+  observer.next('Segundo mensaje');
+  setInterval(() => {
+    observer.next('Nuevo mensaje');
+  },2000);
+})
+
+const observer1 = obs.subscribe(
+  (x: any) => addMessage(x),
+  (e: any) => printMessage(e),
+  () => printMessage('Mensaje completo', 'complete')
+)
+
+obs.subscribe(
+  (x: any) => addMessage(x, true),
+  (e: any) => printMessage(e),
+  () => printMessage('Mensaje completo', 'complete')
+)
+
 setTimeout(() => {
-  // Unsubscribes BOTH subscription and childSubscription
-  subscription.unsubscribe();
+  const coldObs = obs.subscribe((x: any) => addMessage(`${x} Observer 3`))
 }, 3000);
-
-
-// const observable = from([10, 20, 30]);
-const observable = from([
-  {name: 'Cris', last: 'Vega'},
-  {name: 'Henricito', last: 'Rojas'},
-  {name: 'Simba', last: 'Ceballos'}
-]);
-const subs = observable.subscribe(x => printMessage(x.name, 'complete'));
-// Later:
-subs.unsubscribe();
