56f34dfc984e54473aaa0e15f12e7ffd2d4288c4
Cristian <cristian.vega08@gmail.com>
2019-09-08 23:57:21 -0500

    replay with inteval

--- src/code.ts
+++ src/code.ts
@@ -11,9 +11,24 @@ subject.next('Primer mensaje');
 subject.next('Segundo mensaje');
 subject.next('Tercer mensaje');
 subject.next('Cuarto mensaje');
- 
+
 subject.subscribe(
   (x: any) => addMessage(`observerB: ${x}`, true)
 );
 
 subject.next('Quinto Mensaje');
+
+// const subject = new ReplaySubject(100, 500 /* windowTime */);
+
+// subject.subscribe(
+//   (x: any) => addMessage(`observerA: ${x}`)
+// );
+
+// let i = 1;
+// setInterval(() => subject.next(i++), 200);
+
+// setTimeout(() => {
+//   subject.subscribe(
+//     (x:any) => addMessage(`observerB: ${x}`, true)
+//   );
+// }, 1000);
