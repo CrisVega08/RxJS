32b7aff779f1381a1d7aecb1f470bff4ce0f8d15
Cristian <cristian.vega08@gmail.com>
2019-09-09 00:18:13 -0500

    async ready

--- src/code.ts
+++ src/code.ts
@@ -1,34 +1,20 @@
-import { ReplaySubject} from "rxjs";
+import { AsyncSubject } from 'rxjs';
 import { addMessage } from './utils';
 
-const subject = new ReplaySubject(3);
+const subject = new AsyncSubject();
  
 subject.subscribe(
-  (x: any) => addMessage(`observerA: ${x}`)
+  (x:any) => addMessage(`observerA: ${x}`)
 );
  
-subject.next('Primer mensaje');
-subject.next('Segundo mensaje');
-subject.next('Tercer mensaje');
-subject.next('Cuarto mensaje');
-
+subject.next(1);
+subject.next(2);
+subject.next(3);
+subject.next(4);
+ 
 subject.subscribe(
-  (x: any) => addMessage(`observerB: ${x}`, true)
+  (x:any) => addMessage(`observerB: ${x}`, true)
 );
-
-subject.next('Quinto Mensaje');
-
-// const subject = new ReplaySubject(100, 500 /* windowTime */);
-
-// subject.subscribe(
-//   (x: any) => addMessage(`observerA: ${x}`)
-// );
-
-// let i = 1;
-// setInterval(() => subject.next(i++), 200);
-
-// setTimeout(() => {
-//   subject.subscribe(
-//     (x:any) => addMessage(`observerB: ${x}`, true)
-//   );
-// }, 1000);
+ 
+subject.next(5);
+subject.complete();
