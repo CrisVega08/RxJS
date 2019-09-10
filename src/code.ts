1d756d332319f3a1d7d8cba1309fa2bb3de8f4af
Cristian <cristian.vega08@gmail.com>
2019-09-09 19:45:53 -0500

    horizontal scroll

--- src/code.ts
+++ src/code.ts
@@ -1,20 +1,19 @@
-import { AsyncSubject } from 'rxjs';
-import { addMessage } from './utils';
+import { fromEvent } from 'rxjs'; 
+import { throttleTime, tap } from 'rxjs/operators';
 
-const subject = new AsyncSubject();
- 
-subject.subscribe(
-  (x:any) => addMessage(`observerA: ${x}`)
-);
- 
-subject.next(1);
-subject.next(2);
-subject.next(3);
-subject.next(4);
- 
-subject.subscribe(
-  (x:any) => addMessage(`observerB: ${x}`, true)
-);
- 
-subject.next(5);
-subject.complete();
+const scrollIndication = document.getElementById('indication');
+const getScrollWidth = () => {
+  const doc = document.documentElement;
+  const winScroll = doc.scrollTop;
+  const height = doc.scrollHeight - doc.clientHeight;
+  return (winScroll / height) * 100;
+}
+const setScroll = () => 
+  scrollIndication.style.width = getScrollWidth() + '%'
+
+fromEvent(document, 'scroll')
+  .pipe(
+    throttleTime(20),
+    tap(setScroll)
+  )
+.subscribe()
